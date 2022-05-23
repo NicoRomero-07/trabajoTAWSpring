1f5e4827b32d9bbb9c583399006f3ecb3d12da31
pablogodiaz <pablogodiaz+1@gmail.com>
2022-05-23 11:39:21 +0200

    ListaVendedorController

--- src/main/java/es/trabajotaw/trabajotaw/dao/ProductoRepository.java
+++ src/main/java/es/trabajotaw/trabajotaw/dao/ProductoRepository.java
@@ -2,8 +2,13 @@ package es.trabajotaw.trabajotaw.dao;
 
 import es.trabajotaw.trabajotaw.entity.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
+    @Query("select p FROM Producto p where p.publicador.idUsuario = :publicadorid")
+    List<ProductoDTO> listaProductosPublicadorId(@Param("publicadorid") Integer publicadorid);
 }
