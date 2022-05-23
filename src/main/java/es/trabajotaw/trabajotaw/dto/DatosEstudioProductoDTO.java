ded63e07053e1ca554af5700a034ab1587874530
AlfonsoGonzalezOrtiz <80314412+AlfonsoGonzalezOrtiz@users.noreply.github.com>
2022-05-23 11:26:34 +0200

    Añadido los dto

---
+++ src/main/java/es/trabajotaw/trabajotaw/dto/DatosEstudioProductoDTO.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.trabajotaw.trabajotaw.dto;
+
+
+/**
+ *
+ * @author Alfonso
+ */
+public class DatosEstudioProductoDTO {
+    
+    private Integer id;
+    private Boolean categorias;
+    private Boolean vendidos;
+    private Boolean promocion;
+    private Double precioSalida;
+    private Double precioActual;
+    private EstudioDTO estudio;
+    
+    public DatosEstudioProductoDTO() {
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(Boolean categorias) {
+        this.categorias = categorias;
+    }
+
+    public Boolean getVendidos() {
+        return vendidos;
+    }
+
+    public void setVendidos(Boolean vendidos) {
+        this.vendidos = vendidos;
+    }
+
+    public Boolean getPromocion() {
+        return promocion;
+    }
+
+    public void setPromocion(Boolean promocion) {
+        this.promocion = promocion;
+    }
+
+    public Double getPrecioSalida() {
+        return precioSalida;
+    }
+
+    public void setPrecioSalida(Double precioSalida) {
+        this.precioSalida = precioSalida;
+    }
+
+    public Double getPrecioActual() {
+        return precioActual;
+    }
+
+    public void setPrecioActual(Double precioActual) {
+        this.precioActual = precioActual;
+    }
+    
+    public EstudioDTO getEstudio() {
+        return estudio;
+    }
+
+    public void setEstudio(EstudioDTO estudio) {
+        this.estudio = estudio;
+    }
+    
+}
---
+++ src/main/java/es/trabajotaw/trabajotaw/dto/UsuarioDTO.java
@@ -0,0 +1,146 @@
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.trabajotaw.trabajotaw.dto;
+
+
+    import java.util.Date;
+    import java.util.Objects;
+
+    /**
+ *
+ * @author nicor
+ */
+public class UsuarioDTO {
+    private Integer idUsuario;
+    private String nombreUsuario;
+    private String contrasenya;
+    private String email;
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private Date fechaNacimiento;
+    private Character sexo;
+    private DireccionDTO direccion;
+    private TipoUsuarioDTO tipoUsuario;
+ 
+    public UsuarioDTO() {
+    }
+    
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public Character getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Character sexo) {
+        this.sexo = sexo;
+    }
+    
+    public DireccionDTO getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(DireccionDTO direccion) {
+        this.direccion = direccion;
+    }
+
+    public TipoUsuarioDTO getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoUsuarioDTO tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 61 * hash + Objects.hashCode(this.idUsuario);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UsuarioDTO other = (UsuarioDTO) obj;
+        if (!Objects.equals(this.idUsuario, other.idUsuario)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
