b08d6e1e77e282462dbf03407acf6961295ef9b9
AlfonsoGonzalezOrtiz <0619973969@uma.es>
2022-05-27 15:46:55 +0200

    Arreglo de error

--- src/main/java/es/trabajotaw/trabajotaw/controller/EstudiosController.java
+++ src/main/java/es/trabajotaw/trabajotaw/controller/EstudiosController.java
@@ -25,14 +25,14 @@ public class EstudiosController {
     private DatosEstudioUsuarioService estudioUsuarioService;
 
     @GetMapping("analista")
-    public String inicio(Model model, HttpSession session) {
+    public String init(Model model, HttpSession session) {
         List<EstudioDTO> estudios = this.estudioService.listarClientes(null);
         model.addAttribute("estudios", estudios);
         return "estudios";
     }
 
     @GetMapping("analista/filtro")
-    public String filtro(Model model, @RequestParam("filtroNombre") String filtroNombre, HttpSession session) {
+    public String filter(Model model, @RequestParam("filtroNombre") String filtroNombre, HttpSession session) {
         List<EstudioDTO> estudios = this.estudioService.listarClientes(filtroNombre);
         model.addAttribute("filtroNombre", filtroNombre);
         model.addAttribute("estudios", estudios);
@@ -70,7 +70,6 @@ public class EstudiosController {
         model.addAttribute("estudio",estudio);
         DatosEstudioProductoDTO estudioProducto = estudio.getDatosEstudioProducto();
         DatosEstudioUsuarioDTO estudioUsuario = estudio.getDatosEstudioUsuario();
-
         if(estudioProducto != null){
             List<ProductoDTO> listaProductos = this.productoService.visualizarEstudio(estudioProducto.getId());
             model.addAttribute("listaProductos",listaProductos);
@@ -87,7 +86,7 @@ public class EstudiosController {
     */
 
     @GetMapping("analista/edit/{id}")
-    public String save(Model model, @PathVariable String id, HttpSession session){
+    public String edit(Model model, @PathVariable String id, HttpSession session){
         // Cogemos los usuarios analistas y le añadimos los administradores.
         List<UsuarioDTO> listaUsuarios = this.usuarioService.getAnalistas();
         List<UsuarioDTO> listaAdministradores = this.usuarioService.getAdministradores();
@@ -101,166 +100,38 @@ public class EstudiosController {
         return "estudio";
     }
 
-    @PostMapping("analista/save/{id}")
-    public String saveEstudio(Model model, HttpSession session,
-                              @RequestParam String id,@RequestParam String nombre,
-                              @RequestParam String analista,@RequestParam String descripcion,
-                              @RequestParam String element){
-
-        if (id == null || id.isEmpty()) {    // Crear nuevo estudio
-            EstudioDTO estudioDTO = estudioService.save(nombre,analista,descripcion,element,null,null);
-            id = estudioDTO.getIdEstudio().toString();
-        } else {                                   // savear estudio
-            estudioService.save(id,nombre,analista,descripcion,element,null,null);
-        }
-        return "redirect:/analista/save/datosEstudio/" + id;
+    @GetMapping("analista/create")
+    public String create(Model model, HttpSession session){
+        // Cogemos los usuarios analistas y le añadimos los administradores.
+        List<UsuarioDTO> listaUsuarios = this.usuarioService.getAnalistas();
+        List<UsuarioDTO> listaAdministradores = this.usuarioService.getAdministradores();
+        listaUsuarios.addAll(listaAdministradores);
+        model.addAttribute("usuarios", listaUsuarios);
+        return "estudio";
     }
 
-    @GetMapping("analista/edit/datosEstudio/{id}")
-    public String datosEstudio(Model model, @PathVariable String id, HttpSession session){
-        
-        if (id != null && !id.isEmpty()) {
-            EstudioDTO estudio = this.estudioService.getById(Integer.parseInt(id));
-            model.addAttribute("estudio", estudio);
+    @PostMapping("analista/save")
+    public String saveEstudio(Model model, HttpSession session,@ModelAttribute("estudio") EstudioDTO estudio,@RequestParam String element){
 
-            DatosEstudioProductoDTO estudioProducto = null;
-            DatosEstudioUsuarioDTO estudioUsuario = null;
-            
-            if(estudio.getProducto()){
-                estudioProducto = this.estudioProductoService.getById(Integer.parseInt(id));
-            }else{
-                estudioUsuario = this.estudioUsuarioService.getById(Integer.parseInt(id));
-            }
-            if (estudioProducto != null) {
-                model.addAttribute("estudioProducto", estudioProducto);
-            } else if (estudioUsuario != null) {
-                model.addAttribute("estudioUsuario", estudioUsuario);
-            }
-        }
-        return "datosEstudio";
-    }
-
-    @PostMapping("analista/edit/datosEstudio/saveDatosEstudio")
-    public String saveDatosEstudio(Model model,
-                                   @RequestParam String idEstudio,@RequestParam String idEstudioProducto,
-                                   @RequestParam List<String> estudioProducto,@RequestParam String sprecioSalida,
-                                   @RequestParam String sprecioActual,@RequestParam String idEstudioUsuario,
-                                   @RequestParam List<String> estudioUsuario,
-                                   HttpSession session) {
-        EstudioDTO estudio;
-
-        Double precioSalida = sprecioSalida != null && !sprecioSalida.isEmpty() ? Double.parseDouble(sprecioSalida) : null;
-        Double precioActual = sprecioActual != null && !sprecioActual.isEmpty() ? Double.parseDouble(sprecioActual) : null;
-
-        if (precioSalida != null && precioActual != null && precioActual < precioSalida) {
-            return "redirect:/analista/save/datosEstudio/" + idEstudio + "/" + 1;
+        if (estudio == null) {    // Crear nuevo estudio
+            EstudioDTO estudioDTO = estudioService.save(estudio.getNombre(),
+                    estudio.getAnalista().toString(),estudio.getDescripcion(),element,null,null);
         } else {
-            estudio = this.estudioService.getById(Integer.parseInt(idEstudio));
-
-            if (estudio.getProducto()) {
-                Boolean categorias = Boolean.FALSE;
-                Boolean vendidos = Boolean.FALSE;
-                Boolean promocion = Boolean.FALSE;
-
-                if (estudioProducto != null) {
-                    for (String s : estudioProducto) {
-                        if (s.equals("categorias")) {
-                            categorias = Boolean.TRUE;
-                        } else if (s.equals("vendidos")) {
-                            vendidos = Boolean.TRUE;
-                        } else if (s.equals("enPromocion")) {
-                            promocion = Boolean.TRUE;
-                        }
-                    }
-                }
-
-                if ((idEstudioProducto == null || idEstudioProducto.isEmpty()) && estudio.getProducto()) {    // Crear nuevo estudio
-                    estudioProductoService.save(categorias, vendidos,
-                            promocion, precioSalida,
-                            precioActual, idEstudio);
-                } else if (estudio.getProducto()) {                                                                // savear estudio
-                    estudioProductoService.save(idEstudioProducto, categorias,
-                            vendidos, promocion, precioSalida,
-                            precioActual, idEstudio);
-                }
-
-                estudioService.save(estudio.getIdEstudio().toString(), null, null, null, null, idEstudioProducto, null);
-
-            } else {
-                Boolean nombre = Boolean.FALSE;
-                Boolean apellidos = Boolean.FALSE;
-                Boolean ingresos = Boolean.FALSE;
-                Boolean ascendente = Boolean.FALSE;
-
-                if (estudioUsuario != null) {
-                    for (String s : estudioUsuario) {
-                        if (s.equals("nombre")) {
-                            nombre = Boolean.TRUE;
-                        } else if (s.equals("apellidos")) {
-                            apellidos = Boolean.TRUE;
-                        } else if (s.equals("ingresos") || s.equals("gastos")) {
-                            ingresos = Boolean.TRUE;
-                        } else if (s.equals("ascendente")) {
-                            ascendente = Boolean.TRUE;
-                        }
-                    }
-                }
-
-                if ((idEstudioUsuario == null || idEstudioUsuario.isEmpty()) && (estudio.getVendedor() || estudio.getComprador())) {    // Crear nuevo estudio
-                    estudioUsuarioService.save(nombre, apellidos, ingresos, ascendente, idEstudio);
-                } else if (estudio.getVendedor() || estudio.getComprador()) {                                                                // savear estudio
-                    estudioUsuarioService.save(idEstudioUsuario, nombre, apellidos, ingresos, ascendente, idEstudio);
-                }
-                estudioService.save(estudio.getIdEstudio().toString(), null, null, null, null, null, idEstudioUsuario);
-            }
-            return "redirect:/analista/";
+            estudioService.save(estudio.getIdEstudio().toString(),estudio.getNombre(),estudio.getAnalista().toString(),estudio.getDescripcion(),element,null,null);
         }
-
+        return "redirect:/analista/estudio/save/datosEstudio/" + estudio.getIdEstudio();
     }
 
     @GetMapping("analista/estudio/save/datosEstudio/{id}")
-=======
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> main
-    @PostMapping("analista/save/{id}")
-    public String saveEstudio(Model model, HttpSession session,
-                              @RequestParam String id,@RequestParam String nombre,
-                              @RequestParam String analista,@RequestParam String descripcion,
-                              @RequestParam String element){
-
-        if (id == null || id.isEmpty()) {    // Crear nuevo estudio
-            EstudioDTO estudioDTO = estudioService.save(nombre,analista,descripcion,element,null,null);
-            id = estudioDTO.getIdEstudio().toString();
-        } else {                                   // savear estudio
-            estudioService.save(id,nombre,analista,descripcion,element,null,null);
-        }
-        return "redirect:/analista/save/datosEstudio/" + id;
-    }
-
-    @GetMapping("analista/edit/datosEstudio/{id}")
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> main
     public String datosEstudio(Model model, @PathVariable String id, HttpSession session){
-        
+
         if (id != null && !id.isEmpty()) {
             EstudioDTO estudio = this.estudioService.getById(Integer.parseInt(id));
             model.addAttribute("estudio", estudio);
 
             DatosEstudioProductoDTO estudioProducto = null;
             DatosEstudioUsuarioDTO estudioUsuario = null;
-            
+
             if(estudio.getProducto()){
                 estudioProducto = this.estudioProductoService.getById(Integer.parseInt(id));
             }else{
@@ -275,10 +146,7 @@ public class EstudiosController {
         return "datosEstudio";
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     @PostMapping("analista/estudio/save/datosEstudio/save")
     public String saveDatosEstudio(Model model,@ModelAttribute("estudioProducto") DatosEstudioProductoDTO estudioProducto,
                                    @ModelAttribute("estudioUsuario") DatosEstudioUsuarioDTO estudioUsuario,@RequestParam("idEstudio") String idEstudio,
@@ -315,141 +183,6 @@ public class EstudiosController {
                             estudioUsuario.getApellidos(), estudioUsuario.getIngresos(), estudioUsuario.getAscendente(), idEstudio);
                 }
                 estudioService.save(estudio.getIdEstudio().toString(), null, null, null, null, null, estudioUsuario.getId().toString());
-=======
-=======
->>>>>>> main
-    @PostMapping("analista/save/datosEstudio/{id}")
-    public String saveDatosEstudio(Model model,
-                                   @RequestParam String idEstudio,@RequestParam String idEstudioProducto,
-                                   @RequestParam String[] elementsProducto,@RequestParam String sprecioSalida,
-                                   @RequestParam String sprecioActual,@RequestParam String idEstudioUsuario,
-                                   @RequestParam String[] elementsUsuario,
-                                   HttpSession session) {
-        EstudioDTO estudio;
-        DatosEstudioProductoDTO estudioProducto;
-        DatosEstudioUsuarioDTO estudioUsuario;
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-    @PostMapping("analista/edit/datosEstudio/saveDatosEstudio")
-    public String saveDatosEstudio(Model model,
-                                   @RequestParam String idEstudio,@RequestParam String idEstudioProducto,
-                                   @RequestParam List<String> estudioProducto,@RequestParam String sprecioSalida,
-                                   @RequestParam String sprecioActual,@RequestParam String idEstudioUsuario,
-                                   @RequestParam List<String> estudioUsuario,
-                                   HttpSession session) {
-        EstudioDTO estudio;
-<<<<<<< HEAD
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> main
-
-        Double precioSalida = sprecioSalida != null && !sprecioSalida.isEmpty() ? Double.parseDouble(sprecioSalida) : null;
-        Double precioActual = sprecioActual != null && !sprecioActual.isEmpty() ? Double.parseDouble(sprecioActual) : null;
-
-        if (precioSalida != null && precioActual != null && precioActual < precioSalida) {
-            return "redirect:/analista/save/datosEstudio/" + idEstudio + "/" + 1;
-        } else {
-            estudio = this.estudioService.getById(Integer.parseInt(idEstudio));
-
-            if (estudio.getProducto()) {
-                Boolean categorias = Boolean.FALSE;
-                Boolean vendidos = Boolean.FALSE;
-                Boolean promocion = Boolean.FALSE;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                if (elementsProducto != null) {
-                    for (String s : elementsProducto) {
-=======
-                if (estudioProducto != null) {
-                    for (String s : estudioProducto) {
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
-                if (estudioProducto != null) {
-                    for (String s : estudioProducto) {
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
-                if (elementsProducto != null) {
-                    for (String s : elementsProducto) {
->>>>>>> main
-                        if (s.equals("categorias")) {
-                            categorias = Boolean.TRUE;
-                        } else if (s.equals("vendidos")) {
-                            vendidos = Boolean.TRUE;
-                        } else if (s.equals("enPromocion")) {
-                            promocion = Boolean.TRUE;
-                        }
-                    }
-                }
-
-                if ((idEstudioProducto == null || idEstudioProducto.isEmpty()) && estudio.getProducto()) {    // Crear nuevo estudio
-                    estudioProductoService.save(categorias, vendidos,
-                            promocion, precioSalida,
-                            precioActual, idEstudio);
-                } else if (estudio.getProducto()) {                                                                // savear estudio
-                    estudioProductoService.save(idEstudioProducto, categorias,
-                            vendidos, promocion, precioSalida,
-                            precioActual, idEstudio);
-                }
-
-                estudioService.save(estudio.getIdEstudio().toString(), null, null, null, null, idEstudioProducto, null);
-
-            } else {
-                Boolean nombre = Boolean.FALSE;
-                Boolean apellidos = Boolean.FALSE;
-                Boolean ingresos = Boolean.FALSE;
-                Boolean ascendente = Boolean.FALSE;
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                if (elementsUsuario != null) {
-                    for (String s : elementsUsuario) {
-=======
-                if (estudioUsuario != null) {
-                    for (String s : estudioUsuario) {
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
-                if (estudioUsuario != null) {
-                    for (String s : estudioUsuario) {
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
-                if (elementsUsuario != null) {
-                    for (String s : elementsUsuario) {
->>>>>>> main
-                        if (s.equals("nombre")) {
-                            nombre = Boolean.TRUE;
-                        } else if (s.equals("apellidos")) {
-                            apellidos = Boolean.TRUE;
-                        } else if (s.equals("ingresos") || s.equals("gastos")) {
-                            ingresos = Boolean.TRUE;
-                        } else if (s.equals("ascendente")) {
-                            ascendente = Boolean.TRUE;
-                        }
-                    }
-                }
-
-                if ((idEstudioUsuario == null || idEstudioUsuario.isEmpty()) && (estudio.getVendedor() || estudio.getComprador())) {    // Crear nuevo estudio
-                    estudioUsuarioService.save(nombre, apellidos, ingresos, ascendente, idEstudio);
-                } else if (estudio.getVendedor() || estudio.getComprador()) {                                                                // savear estudio
-                    estudioUsuarioService.save(idEstudioUsuario, nombre, apellidos, ingresos, ascendente, idEstudio);
-                }
-                estudioService.save(estudio.getIdEstudio().toString(), null, null, null, null, null, idEstudioUsuario);
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> main
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> 71a343530470d070538feb24bdfa57a238b9d4dc
-=======
->>>>>>> main
             }
             return "redirect:/analista/";
         }
